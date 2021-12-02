3e09ac4d8be8a9c1cf81c5e8092289ad64d63bc7
Oskar Thornblad <oskar.thornblad@gmail.com>
2021-12-02 22:27:10 +0100

    Add day1 part2

--- src/day1.rs
+++ src/day1.rs
@@ -6,6 +6,11 @@ pub fn input_generator(input: &str) -> Depth {
 }
 
 #[aoc(day1, part1)]
-pub fn part1(depth: &Depth) -> usize {
+pub fn part1(depth: &Depth) -> u64 {
     depth.get_increases()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(depth: &Depth) -> u64 {
+    depth.get_windowed_increases()
 }
--- src/sonar.rs
+++ src/sonar.rs
@@ -15,21 +15,54 @@ impl From<&str> for Depth {
     }
 }
 
-impl Depth {
-    pub fn get_increases(&self) -> usize {
-        let mut increases = 0usize;
-        let mut opt_last = None;
-        for depth in &self.depths {
-            if let Some(l) = opt_last {
-                if depth > l {
-                    increases += 1;
-                }
-            }
+fn get_window(depths: &Vec<u64>) -> u64 {
+    let mut depth = 0;
+    if let Some(v) = depths.get(0) {
+        depth += v
+    }
+    if let Some(v) = depths.get(1) {
+        depth += v
+    }
+    if let Some(v) = depths.get(2) {
+        depth += v
+    }
 
-            opt_last = Some(depth);
+    depth
+}
+
+fn get_windows(depths: &Vec<u64>) -> Vec<u64> {
+    let mut windows = vec![];
+
+    for i in 0..depths.len() {
+        windows.push(get_window(&depths.iter().skip(i).map(|v| *v).collect()));
+    }
+
+    windows
+}
+
+fn get_increases(depths: &Vec<u64>) -> u64 {
+    let mut increases = 0u64;
+    let mut opt_last = None;
+    for depth in depths {
+        if let Some(l) = opt_last {
+            if depth > l {
+                increases += 1;
+            }
         }
 
-        increases
+        opt_last = Some(depth);
+    }
+
+    increases
+}
+
+impl Depth {
+    pub fn get_increases(&self) -> u64 {
+        get_increases(&self.depths)
+    }
+
+    pub fn get_windowed_increases(&self) -> u64 {
+        get_increases(&get_windows(&self.depths))
     }
 }
 
@@ -52,5 +85,22 @@ mod tests {
         let depth = Depth::from(input);
 
         assert_eq!(depth.get_increases(), 7);
+    }
+
+    #[test]
+    fn test_windowed_increases() {
+        let input = "199\n\
+                           200\n\
+                           208\n\
+                           210\n\
+                           200\n\
+                           207\n\
+                           240\n\
+                           269\n\
+                           260\n\
+                           263";
+        let depth = Depth::from(input);
+
+        assert_eq!(depth.get_windowed_increases(), 5);
     }
 }
