dad5e2d7d3bea0cc8797272cc58c452f79510fb0
Oskar Thornblad <oskar.thornblad@gmail.com>
2021-12-10 16:50:37 +0100

    Invalid day5 part2 for some reason

--- src/day5.rs
+++ src/day5.rs
@@ -9,3 +9,8 @@ pub fn input_generator(input: &str) -> Map {
 pub fn part1(map: &Map) -> usize {
     map.get_count_without_diagonals()
 }
+
+#[aoc(day5, part2)]
+pub fn part2(map: &Map) -> usize {
+    map.get_count_with_diagonals()
+}
