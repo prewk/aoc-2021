4fee15adbce02e65d3c4435985309188676d461d
Oskar Thornblad <oskar.thornblad@gmail.com>
2021-12-23 20:16:50 +0100

    day9 part2

--- src/day9.rs
+++ src/day9.rs
@@ -9,3 +9,8 @@ pub fn input_generator(input: &str) -> Map {
 pub fn part1(map: &Map) -> u32 {
     risk_sum(&map)
 }
+
+#[aoc(day9, part2)]
+pub fn part2(map: &Map) -> usize {
+    map.mult_three_largest_basins()
+}
--- src/smoke.rs
+++ src/smoke.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, HashMap};
+
 #[derive(Debug, PartialEq)]
 pub struct Map {
     tiles: Vec<Vec<u32>>,
@@ -58,14 +60,90 @@ impl Map {
 
         low_points
     }
+
+    pub fn detect_basin(&self, x: usize, y: usize, inc_visited: &HashMap<(usize, usize), bool>) -> HashMap<(usize, usize), bool> {
+        let window = self.peek(x, y).expect("Invalid coords");
+
+        let mut visited = inc_visited.clone();
+
+        if window.center == 9 {
+            visited.insert((x, y), false);
+
+            return visited;
+        } else {
+            visited.insert((x, y), true);
+        }
+
+        for (dir, pos) in [
+            (&window.north, if y > 0 { Some((x, y - 1)) } else { None }),
+            (&window.east, Some((x + 1, y))),
+            (&window.south, Some((x, y + 1))),
+            (&window.west, if x > 0 { Some((x - 1, y)) } else { None })
+        ] {
+            if let Some((it_x, it_y)) = pos {
+                if let Some(n) = *dir {
+                    if visited.contains_key(&(it_x, it_y)) {
+                        continue;
+                    }
+
+                    if n < 9 {
+                        for ((rec_x, rec_y), in_basin) in self.detect_basin(it_x, it_y, &visited) {
+                            visited.insert((rec_x, rec_y), in_basin);
+                        }
+                    } else {
+                        visited.insert((it_x, it_y), false);
+                    }
+                }
+            }
+        }
+
+        visited
+    }
+
+    pub fn find_basins(&self) -> Vec<usize> {
+        let mut basins = vec![];
+        let mut visited = HashMap::new();
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if visited.contains_key(&(x, y)) { continue; }
+
+                let detected = self.detect_basin(x, y, &HashMap::new());
+
+                let mut total = 0;
+
+                for ((d_x, d_y), in_basin) in &detected {
+                    visited.insert((*d_x, *d_y), *in_basin);
+
+                    if *in_basin {
+                        total += 1;
+                    }
+                }
+
+                if total > 0 {
+                    basins.push(total);
+                }
+            }
+        }
+
+        basins
+    }
+
+    pub fn mult_three_largest_basins(&self) -> usize {
+        let mut basins = self.find_basins();
+
+        basins.sort();
+
+        basins.iter().rev().take(3).fold(1, |prod, b| b * prod)
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Window {
-    north: Option<u32>,
-    east: Option<u32>,
-    south: Option<u32>,
-    west: Option<u32>,
+    pub north: Option<u32>,
+    pub east: Option<u32>,
+    pub south: Option<u32>,
+    pub west: Option<u32>,
 
     pub center: u32,
 }
@@ -100,5 +178,46 @@ mod tests {
                 9899965678");
 
         assert_eq!(risk_sum(&map), 15);
+    }
+
+    #[test]
+    fn test_basin_detector() {
+        let map = Map::from("2199943210\n\
+                3987894921\n\
+                9856789892\n\
+                8767896789\n\
+                9899965678");
+
+        let possible = map.detect_basin(0, 0, &HashMap::new());
+
+        assert_eq!(possible.len(), 6);
+        assert_eq!(possible.get(&(0, 0)), Some(&true));
+        assert_eq!(possible.get(&(1, 0)), Some(&true));
+        assert_eq!(possible.get(&(2, 0)), Some(&false));
+        assert_eq!(possible.get(&(0, 1)), Some(&true));
+        assert_eq!(possible.get(&(1, 1)), Some(&false));
+        assert_eq!(possible.get(&(0, 2)), Some(&false));
+    }
+
+    #[test]
+    fn test_find_basins() {
+        let map = Map::from("2199943210\n\
+                3987894921\n\
+                9856789892\n\
+                8767896789\n\
+                9899965678");
+
+        assert_eq!(map.find_basins(), vec![3, 9, 14, 9])
+    }
+
+    #[test]
+    fn test_mult_three_largest_basins() {
+        let map = Map::from("2199943210\n\
+                3987894921\n\
+                9856789892\n\
+                8767896789\n\
+                9899965678");
+
+        assert_eq!(map.mult_three_largest_basins(), 1134);
     }
 }
