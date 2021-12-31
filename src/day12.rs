fd7eb119c9c4490541d165739b9c0c7d6637a859
Oskar Thornblad <oskar.thornblad@gmail.com>
2021-12-31 22:01:51 +0100

    Day12 part2

--- src/day12.rs
+++ src/day12.rs
@@ -7,3 +7,8 @@ pub fn input_generator(input: &str) -> Map { Map::from(input) }
 pub fn part1(map: &Map) -> usize {
     map.paths().unwrap().len()
 }
+
+#[aoc(day12, part2)]
+pub fn part2(map: &Map) -> usize {
+    map.paths_allow_twice().unwrap().len()
+}
--- src/path.rs
+++ src/path.rs
@@ -66,11 +66,11 @@ impl From<&str> for Map {
         for passage in input.lines().map(Passage::from) {
             lookup
                 .entry(passage.left.clone())
-                .or_insert(HashSet::new())
+                .or_insert_with(HashSet::new)
                 .insert(passage.right.clone());
             lookup
                 .entry(passage.right.clone())
-                .or_insert(HashSet::new())
+                .or_insert_with(HashSet::new)
                 .insert(passage.left.clone());
         }
 
@@ -83,7 +83,7 @@ impl Map {
         &self,
         subject: &Cave,
         visited_small: HashSet<String>,
-        depth: usize,
+        allow_twice: Option<&str>
     ) -> Result<Vec<Vec<Cave>>> {
         let mut paths = vec![];
         let caves = self.lookup.get(subject).context("Missing cave")?;
@@ -101,10 +101,23 @@ impl Map {
                         continue 'caves;
                     }
 
-                    visited_small.insert(c.clone());
+                    if let Some(allowee) = allow_twice {
+                        if &allowee.to_string() == c {
+                            if visited_small.contains(&format!("{}-twice", c)) {
+                                visited_small.insert(c.clone());
+                            } else {
+                                visited_small.insert(format!("{}-twice", c));
+                            }
+                        } else {
+                            visited_small.insert(c.clone());
+                        }
+                    } else {
+                        visited_small.insert(c.clone());
+                    }
+
 
                     for children in self
-                        .visit(cave, visited_small.clone(), depth + 1)?
+                        .visit(cave, visited_small.clone(), allow_twice,)?
                         .iter_mut()
                     {
                         let mut this_and_children = vec![Cave::Small(c.clone())];
@@ -118,7 +131,7 @@ impl Map {
                 }
                 Cave::Large(c) => {
                     for children in self
-                        .visit(cave, visited_small.clone(), depth + 1)?
+                        .visit(cave, visited_small.clone(), allow_twice,)?
                         .iter_mut()
                     {
                         let mut this_and_children = vec![Cave::Large(c.clone())];
@@ -136,15 +149,33 @@ impl Map {
         Ok(paths)
     }
 
-    pub fn paths(&self) -> Result<Vec<Vec<Cave>>> {
-        let mut paths = vec![];
+    pub fn paths(&self) -> Result<HashSet<String>> {
+        let mut paths = HashSet::new();
 
-        for visit in self.visit(&Cave::Start, HashSet::new(), 0)? {
+        for visit in self.visit(&Cave::Start, HashSet::new(), None)? {
             let mut this_and_children = vec![Cave::Start];
 
             this_and_children.extend(visit);
 
-            paths.push(this_and_children);
+            paths.insert(path_to_string(&this_and_children));
+        }
+
+        Ok(paths)
+    }
+
+    pub fn paths_allow_twice(&self) -> Result<HashSet<String>> {
+        let mut paths = HashSet::new();
+
+        for allowee in self.lookup.keys() {
+            if let Cave::Small(sm_cave) = allowee {
+                for visit in self.visit(&Cave::Start, HashSet::new(), Some(sm_cave))? {
+                    let mut this_and_children = vec![Cave::Start];
+
+                    this_and_children.extend(visit);
+
+                    paths.insert(path_to_string(&this_and_children));
+                }
+            }
         }
 
         Ok(paths)
@@ -162,7 +193,6 @@ pub fn path_to_string(caves: &Vec<Cave>) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::iter::FromIterator;
 
     #[test]
     fn test_paths_small() {
@@ -176,13 +206,7 @@ mod tests {
             b-end",
         );
 
-        let all: HashSet<String> = HashSet::from_iter(
-            map.paths()
-                .unwrap()
-                .iter()
-                .map(path_to_string)
-                .collect::<Vec<String>>(),
-        );
+        let all = map.paths().unwrap();
 
         assert_eq!(all.len(), 10);
         assert!(all.contains("start,A,b,A,c,A,end"));
@@ -194,6 +218,59 @@ mod tests {
         assert!(all.contains("start,A,end"));
         assert!(all.contains("start,b,A,c,A,end"));
         assert!(all.contains("start,b,A,end"));
+        assert!(all.contains("start,b,end"));
+    }
+
+    #[test]
+    fn test_paths_small_part2() {
+        let map = Map::from(
+            "start-A\n\
+            start-b\n\
+            A-c\n\
+            A-b\n\
+            b-d\n\
+            A-end\n\
+            b-end",
+        );
+
+        let all = map.paths_allow_twice().unwrap();
+
+        assert_eq!(all.len(), 36);
+        assert!(all.contains("start,A,b,A,b,A,c,A,end"));
+        assert!(all.contains("start,A,b,A,b,A,end"));
+        assert!(all.contains("start,A,b,A,b,end"));
+        assert!(all.contains("start,A,b,A,c,A,b,A,end"));
+        assert!(all.contains("start,A,b,A,c,A,b,end"));
+        assert!(all.contains("start,A,b,A,c,A,c,A,end"));
+        assert!(all.contains("start,A,b,A,c,A,end"));
+        assert!(all.contains("start,A,b,A,end"));
+        assert!(all.contains("start,A,b,d,b,A,c,A,end"));
+        assert!(all.contains("start,A,b,d,b,A,end"));
+        assert!(all.contains("start,A,b,d,b,end"));
+        assert!(all.contains("start,A,b,end"));
+        assert!(all.contains("start,A,c,A,b,A,b,A,end"));
+        assert!(all.contains("start,A,c,A,b,A,b,end"));
+        assert!(all.contains("start,A,c,A,b,A,c,A,end"));
+        assert!(all.contains("start,A,c,A,b,A,end"));
+        assert!(all.contains("start,A,c,A,b,d,b,A,end"));
+        assert!(all.contains("start,A,c,A,b,d,b,end"));
+        assert!(all.contains("start,A,c,A,b,end"));
+        assert!(all.contains("start,A,c,A,c,A,b,A,end"));
+        assert!(all.contains("start,A,c,A,c,A,b,end"));
+        assert!(all.contains("start,A,c,A,c,A,end"));
+        assert!(all.contains("start,A,c,A,end"));
+        assert!(all.contains("start,A,end"));
+        assert!(all.contains("start,b,A,b,A,c,A,end"));
+        assert!(all.contains("start,b,A,b,A,end"));
+        assert!(all.contains("start,b,A,b,end"));
+        assert!(all.contains("start,b,A,c,A,b,A,end"));
+        assert!(all.contains("start,b,A,c,A,b,end"));
+        assert!(all.contains("start,b,A,c,A,c,A,end"));
+        assert!(all.contains("start,b,A,c,A,end"));
+        assert!(all.contains("start,b,A,end"));
+        assert!(all.contains("start,b,d,b,A,c,A,end"));
+        assert!(all.contains("start,b,d,b,A,end"));
+        assert!(all.contains("start,b,d,b,end"));
         assert!(all.contains("start,b,end"));
     }
 
@@ -212,13 +289,7 @@ mod tests {
             kj-dc",
         );
 
-        let all: HashSet<String> = HashSet::from_iter(
-            map.paths()
-                .unwrap()
-                .iter()
-                .map(path_to_string)
-                .collect::<Vec<String>>(),
-        );
+        let all = map.paths().unwrap();
 
         assert_eq!(all.len(), 19);
         assert!(all.contains("start,HN,dc,HN,end"));
@@ -240,6 +311,24 @@ mod tests {
         assert!(all.contains("start,kj,HN,end"));
         assert!(all.contains("start,kj,dc,HN,end"));
         assert!(all.contains("start,kj,dc,end"));
+    }
+
+    #[test]
+    fn test_paths_medium_part2() {
+        let map = Map::from(
+            "dc-end\n\
+            HN-start\n\
+            start-kj\n\
+            dc-start\n\
+            dc-HN\n\
+            LN-dc\n\
+            HN-end\n\
+            kj-sa\n\
+            kj-HN\n\
+            kj-dc",
+        );
+
+        assert_eq!(map.paths_allow_twice().unwrap().len(), 103);
     }
 
     #[test]
@@ -265,14 +354,34 @@ mod tests {
             start-RW",
         );
 
-        let all: HashSet<String> = HashSet::from_iter(
-            map.paths()
-                .unwrap()
-                .iter()
-                .map(path_to_string)
-                .collect::<Vec<String>>(),
-        );
+        let all = map.paths().unwrap();
 
         assert_eq!(all.len(), 226);
+    }
+
+    #[test]
+    fn test_paths_large_part2() {
+        let map = Map::from(
+            "fs-end\n\
+            he-DX\n\
+            fs-he\n\
+            start-DX\n\
+            pj-DX\n\
+            end-zg\n\
+            zg-sl\n\
+            zg-pj\n\
+            pj-he\n\
+            RW-he\n\
+            fs-DX\n\
+            pj-RW\n\
+            zg-RW\n\
+            start-pj\n\
+            he-WI\n\
+            zg-he\n\
+            pj-fs\n\
+            start-RW",
+        );
+
+        assert_eq!(map.paths_allow_twice().unwrap().len(), 3509);
     }
 }
